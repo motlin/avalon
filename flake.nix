86684b3c49fcba526cc3aa2e55bbe92ebdb81daf
George Shammas <george@shamm.as>
2023-10-14 16:58:27 -0400

    Add flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,25 @@
 {
-  description = "A very basic flake";
+  description = "Avalon game server Flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
 
   };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = rec {
+          avalon-online = pkgs.callPackage ./default.nix { };
+          default = avalon-online;
+        };
+      }
+    );
 }
